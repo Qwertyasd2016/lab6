06800a81e554a88fbda858025c7e754a17f0bfa3
Qwertyasd2016 <xenjin1323@gmail.com>
2016-10-11 09:56:20 +0300

    2

--- 2.py
+++ 2.py
@@ -1,10 +1,8 @@
 import random
-import matplotlib.pyplot as plt
 
 random.seed(0)
 n = 10000000
-x = [random.uniform(-3, 3) for i in range(n)]
+x = [random.uniform(-2, 2) for i in range(n)]
 f = [-1*x[i]*x[i] + 4 for i in range(len(x))]
-integral = (sum(f)*6)/n
+integral = (sum(f)*4)/n
 print(integral)
-plt.show()
